aa235810e84cdc89e934ffae107d4b1257a25eca
TipDevs <tipdevs@gmail.com>
2025-05-22 18:45:58 +0100

    Add new feature which control the game flow, player turn, wins and draw

--- script.js
+++ script.js
@@ -9,3 +9,48 @@
     const clearBoard = () => board = ['', '', '', '', '', '', '', '', ''];
     return { getGameBoard, updateBoard, clearBoard };
 })();
+
+function createPlayer(name, marker) {
+    const getName = () => name;
+    const getMarker = () => marker;
+    return { getName, getMarker };
+}
+
+(function gameController() {
+    let player1;
+    let player2;
+    let currentPlayer;
+    let gameOver;
+
+    const winningCombo = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],//row combo
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],//column combo
+        [0, 4, 8], [2, 4, 6] //diagonal combo
+    ];
+    const checkWin = (boardToCheck, playerMarker) => {
+        return winningCombo.some(combo => {
+            return combo.every(index => boardToCheck[index] === playerMarker);
+        });
+    };
+    const checkDraw = (boardToCheck => !boardToCheck.includes(''));
+    const switchPlayer = () => {
+        currentPlayer = (currentPlayer === player1) ? player2 : player1
+    };
+
+    const getPlayerMove = () => {
+        let move;
+        let isValidMove = false;
+        while (!isValidMove) {
+            const moveInput = prompt(`${currentPlayer.name}, enter your move (1-9)`);
+            move = parseInt(moveInput, 10) - 1;
+            if (isNaN(move) || move < 0 || move > 8 || gameBoard.getGameBoard()[move] !== '') {
+                console.log('Invalid move.')
+            }
+            else {
+                isValidMove = true;
+            }
+            return move;
+        }
+    };
+
+})();
